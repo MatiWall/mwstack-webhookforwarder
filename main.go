5e4cf58a145e2b609742b171ea457717a4f30c35
Matias Wallenius <MatiWall@live.dk>
2026-02-04 18:09:07 +0100

    buggfixed and added argoevent url

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
@@ -12,6 +13,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+
+var argoeventsWebhook string = "http://webhook-eventsource-svc.argo.svc.cluster.local:12000/build" // FQDN for webhook service
 
 func readSecret() []byte {
 	dat, err := os.ReadFile("webhook-secret.txt")
@@ -32,11 +36,11 @@ func test(c *gin.Context) {
 	c.String(http.StatusOK, "Working")
 }
 
-func verifySignature(c *gin.Context) bool {
+func verifySignature(c *gin.Context) ([]byte, bool) {
 	secret, exists := c.Get("github-webhook-secret")
 	if !exists {
 		fmt.Println("Secret does not exists")
-		return false
+		return nil, false
 	}
 
 	secret = string(secret.([]uint8))
@@ -44,14 +48,14 @@ func verifySignature(c *gin.Context) bool {
 	gotHash := strings.Split(c.GetHeader("X-Hub-Signature-256"), "=")
 
 	if gotHash[0] != "sha256" {
-		return false
+		return nil, false
 	}
 
 	body, err := io.ReadAll(c.Request.Body)
 	fmt.Println(body)
 	if err != nil {
 		fmt.Println("Failed reading body when verifying signature")
-		return false
+		return nil, false
 	}
 
 	hash := hmac.New(sha256.New, []byte(secret.(string)))
@@ -59,25 +63,29 @@ func verifySignature(c *gin.Context) bool {
 	_, err = hash.Write(body)
 	if err != nil {
 		fmt.Println("Failed to compute hmac")
-		return false
+		return nil, false
 	}
 
 	expectedHash := hex.EncodeToString(hash.Sum(nil))
 	receivedHash := gotHash[1]
-	return receivedHash == expectedHash
+	return body, receivedHash == expectedHash
 
 }
 
 func forwardWebhook(c *gin.Context) {
-	if !verifySignature(c) {
+
+	body, ok := verifySignature(c)
+	if !ok {
 		c.AbortWithStatus(401)
 	}
-
-	c.String(http.StatusOK, "<div>Hello</div>")
+	fmt.Println("Forwarding message: \n %s",  string(body[:]))
+	
+	http.Post(argoeventsWebhook, "application/json",  bytes.NewReader(body))
+	c.String(http.StatusOK, "Successfully forwarded")
 }
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Starting webhook forwarder")
 	secret := readSecret()
 	//fmt.Println(string(secret[:]))
 	router := gin.Default()
@@ -85,6 +93,6 @@ func main() {
 
 	router.Use(apiMiddleware(secret))
 	router.POST("/forward", forwardWebhook)
-	router.GET("", test)
+	router.GET("/", test)
 	router.Run("0.0.0.0:8080")
 }
